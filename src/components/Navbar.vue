3fd41fe848e61f4ef1bd14f6fee5293c60732605
xelinel32 <artemsedlyar99@gmail.com>
2020-03-30 15:57:14 +0300

    create navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="blue darken-3">
+    <div class="nav-wrapper">
+      <router-link to="/" class="brand-logo">Tasks</router-link>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <router-link tag="li" to="/" exact active-class="active"
+          ><a href="sass.html">Create</a></router-link
+        >
+        <router-link tag="li" to="/list" active-class="active"
+          ><a href="sass.html">List</a></router-link
+        >
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+nav { 
+  padding: 0 25px;
+}
+</style>
