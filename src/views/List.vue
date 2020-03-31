765424f5a2948b3a10afa696a957aab2ee813efa
xelinel32 <artemsedlyar99@gmail.com>
2020-03-31 20:51:10 +0300

    create template for receiving data

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,15 +1,52 @@
 <template>
   <div>
-      <h1>List</h1>
+    <h1>List</h1>
+
+    <hr />
+
+    <table v-if="tasks.length">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Date</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th>Open</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, idx) of tasks" :key="task.id">
+          <td>{{ idx + 1 }}</td>
+          <td>{{ task.title }}</td>
+          <td>{{ new Date(task.date).toLocaleDateString() }}</td>
+          <td>
+            <div>{{ task.description }}</div>
+          </td>
+          <td>{{ task.status }}</td>
+          <td>
+            <router-link
+              tag="button"
+              class="btn btn-small"
+              :to="'/task/' + task.id"
+              >Open</router-link
+            >
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <p v-else>Task list empty</p>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  computed: {
+    tasks() {
+      return this.$store.getters.tasks;
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
